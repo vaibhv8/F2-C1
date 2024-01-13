3e1897fbf08a69e5747808fa9c647ad840419826
vaibhv8 <vaibhavwadafale81@gmail.com>
2024-01-13 16:27:16 +0530

    version 0.1

---
+++ script.js
@@ -0,0 +1,41 @@
+let arr = [
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+  
+  function PrintDeveloperbyMap() {
+    arr
+    .filter((employee) => employee.profession === "developer")
+    .map((developer) => console.log(developer));
+  }
+  
+  function PrintDeveloperbyForEach() {
+    arr.forEach((employee) => {
+        if (employee.profession === "developer") {
+          console.log(employee);
+        }
+      });
+  }
+  
+  function addData() {
+    const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+    arr.push(newEmployee);
+    console.log(newEmployee);
+  }
+  
+  function removeAdmin() {
+    const filteredArr = arr.filter((employee) => employee.profession !== "admin");
+    console.log(filteredArr);
+  }
+  
+  function concatenateArray() {
+    const newArray = [
+        { id: 4, name: "alice", age: "22", profession: "designer" },
+        { id: 5, name: "bob", age: "25", profession: "analyst" },
+        { id: 6, name: "dave", age: "28", profession: "manager" },
+      ];
+    
+      const concatenatedArray = arr.concat(newArray);
+      console.log(concatenatedArray);
+  }
